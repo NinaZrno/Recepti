c71b7933bb57b52ed4e10b93f846677889f18869
Nina Zrno <ninaradakovic1234@icloud.com>
2025-02-10 20:40:27 +0100

    update

---
+++ Backend/Controllers/HtppMetodeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/v2/[controller]")]
+    public class HtppMetodeController : ControllerBase
+    {
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
