a502fc1981765cf6308dccc1ba8d9359d95b6c38
Nina Zrno <ninaradakovic1234@icloud.com>
2025-02-10 21:01:10 +0100

    10.02.nastava

---
+++ Backend/Models/Sastojak.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    internal class Sastojak : Entitet
+    {
+        public string Naziv { get; set; }
+        public string Kolicina { get; set; }
+        public string Mjera { get; set; }
+            
+
+    }
+}
