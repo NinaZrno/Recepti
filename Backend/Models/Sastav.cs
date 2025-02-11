36126505f5ae1aebb45b4431c925f24c071e032b
Nina Zrno <ninaradakovic1234@icloud.com>
2025-02-11 20:16:45 +0100

    ...

---
+++ Backend/Models/Sastav.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend.Models
+{
+    
+    public class Sastav : Recept
+    {
+
+        public int? Sifra { get; set; }
+        public string Naziv { get; set; }
+
+        public string Sastojak { get; set; }
+
+        public decimal Kolicina { get; set; }
+
+        public string Napomena { get; set; }
+
+
+    }
+}
--- Backend/Models/Sastojak.cs
+++ Backend/Models/Sastojak.cs
@@ -1,12 +1,22 @@
 ﻿namespace Backend.Models
 {
-    internal class Sastojak : Entitet
+    internal class Sastojak : Recept
     {
        public int? Sifra { get; set; }
         public string Naziv { get; set; }
-        public string Kolicina { get; set; }
+        public string Podrijetlo { get; set; }
         public string Mjera { get; set; }
-            
+        public decimal Energija { get; set; }
+        public decimal Ugljikohidrati { get; set; }
+        public decimal Masti { get; set; }
+        public decimal Zasiceni_seceri { get; set; }
+        public decimal Vlakna { get; set; }
+        public decimal Bjelancevine { get; set; }
+        public decimal Sol { get; set; }
+
+
+
+
 
     }
 }
