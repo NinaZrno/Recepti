3d92c0f5e2aa851577877dd5e4863bd681417a77
Nina Zrno <ninaradakovic1234@icloud.com>
2025-03-04 19:05:28 +0100

    s

---
+++ Backend/Controllers/GlavniController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class GlavniController
+    {
+        public abstract class GlavniController : ControllerBase
+        {
+
+            // dependecy injection
+            // 1. definiraš privatno svojstvo
+            protected readonly BackendContext _context;
+
+            protected readonly IMapper _mapper;
+
+
+            // dependecy injection
+            // 2. proslijediš instancu kroz konstruktor
+            public GlavniController(BackendContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+        }
+    }
+}
