4a445fe00ae6e87c84f2a2b5d8e8c16ce73b3d38
Marta Došen <ninaradakovic1234@icloud.com>
2025-03-11 19:41:46 +0100

    nesto za frontend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,6 +4,11 @@ export const RouteNames={
     RECEPT_PREGLED: '/recepti',
     RECEPT_NOVI: '/recepti/dodaj',
     RECEPT_PROMJENA: '/recepti/:sifra'
+
+    SASTOJAK_PREGLED: '/sastojci',
+    SASTOJAK_NOVI: '/sastojci/dodaj',
+    SASTOJAK_PROMJENA: '/sastojci/:sifra'
+
 }
 
 export const PRODUKCIJA='https://ninazrno-001-site1.ktempurl.com';
--- frontend/src/pages/Pocetna.jsx
+++ frontend/src/pages/Pocetna.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import slikazanaslovnu from '...assets/slikazanaslovnu.jpeg';
+import '../App.css';
+
+
+
 
 
 export default function Pocetna(){
-    
+    const backgroundStyle = {
+        backgroundImage: `url(${slikazanaslovnu})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '80vh',
+        width: '84vw'
+
+   
     return(
         <>
         Dobrodošli na moju aplikaciju
